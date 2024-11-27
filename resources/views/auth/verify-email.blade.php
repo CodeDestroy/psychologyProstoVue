a2fd20769b84bbaa2c22c0bf8cbeb185291fd6c6
CodeDestroy <andrei_novichihin@mail.ru>
2024-11-28 01:50:28 +0300

    email verification

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,9 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div>Спасибо за регистрацию</div>
-<p>Последний шаг. Подтвердите свою учётную запись по ссылке из письма, отправленного на вашу электронную почту.</p>
-<form method="POST" action="{{route('verification.send')}}">
-<button type="submit">Отправить заного</button>
-</form>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+
+                    <div class="card-body">
+                        @if (session('resent'))
+                            <div class="alert alert-success" role="alert">
+                                {{ __('A fresh verification link has been sent to your email address.') }}
+                            </div>
+                        @endif
+
+                        {{ __('Before proceeding, please check your email for a verification link.') }}
+                        {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
