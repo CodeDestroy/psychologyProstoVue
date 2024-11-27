60b0e374f9cfb4452f61b1f5afa9d338ebb7c42c
Andrey <andrei_novichihin@mail.ru>
2024-11-27 19:13:14 +0300

    smtp setup

--- resources/views/education/events/vebinar.blade.php
+++ resources/views/education/events/vebinar.blade.php
@@ -3,11 +3,24 @@
 @section('content')
 @php
     $user = Auth::user();
+    /* $isModer = false; */
+    $roles = $user->roles()->get();
+    $roles = $roles->pluck('slug')->toArray();
+    if (in_array('manager', $roles)) {
+        /* print_r('moder = true'); */
+        /* $isModer = true; */
+        $user->isModer = true;
+    }
 @endphp
-<div class="mx-auto max-w-7xl px-6 lg:px-8">
-    {{$vebinar}}
+<div class="mx-auto max-w-7xl px-6 lg:px-8" style="height: 60vh">
+    {{-- {{$vebinar}} --}}
+    {{-- {{print_r($roles)}}
+    {{$isModer ? 'true' : 'false'}} --}}
     <div id="jitsi">
-        <jitsi :user="{{ json_encode($user) }}" :room="{{ json_encode($vebinar) }}"/>
+        <jitsi 
+            :user="{{ json_encode($user) }}" 
+            :room="{{ json_encode($vebinar) }}" 
+        />
     </div>
 </div>
 @endsection
